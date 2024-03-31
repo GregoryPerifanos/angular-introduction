da499d02c7830230f01a404174aa7eb588f92db8
GregoryPerifanos <g.perifanos89@gmail.com>
2024-03-31 21:10:38 +0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
-import { EventBingExampleComponent } from './components/event-bing-example/event-bing-example.component';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBingExampleComponent],
+  imports: [RouterLink,
+     RouterOutlet, 
+     PersonTableComponent, EventBindExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Gregory';
+ 
 
   person0 = {
      givenName: 'Gregory',
